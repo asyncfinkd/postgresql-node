203bcd470b21cceac1fdb6c223c7968ecfac600d
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-12 03:25:44 +0400

    in folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const pool = require('./db')
+const pool = require('./db/db')
 
 // Middleware (cors)
 app.use(cors())
